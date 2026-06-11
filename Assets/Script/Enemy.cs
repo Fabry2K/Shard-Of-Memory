32b15a6d870dacf9f5ec2c4dc1e2246b36405d82
Fabrizio <fabrizio.orsini04@gmail.com>
2026-06-11 19:16:38 +0200

    added skeleton enemy

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -14,6 +14,15 @@ public class Enemy : MonoBehaviour
 
     protected float recoilTimer;
     protected Rigidbody2D rb;
+
+    protected enum EnemyStates
+    {
+        //skeleton
+        Skeleton_Idle,
+        Skeleton_Flip
+    }
+    protected EnemyStates currentEnemyState;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected virtual void Start()
     {
@@ -24,6 +33,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
+        UpdateEnemyStates();
+
         if(health <= 0)
         {
             Destroy(gameObject);
@@ -60,6 +71,16 @@ public class Enemy : MonoBehaviour
             Attack();
             PlayerController.Instance.HitStopTime(0, 5, 0.3f);
         }
+    }
+
+    protected virtual void UpdateEnemyStates()
+    {
+
+    }
+
+    protected void ChangeState(EnemyStates _newState)
+    {
+        currentEnemyState = _newState;
     }
 
     protected virtual void Attack()
--- Assets/Script/Skeleton.cs
+++ Assets/Script/Skeleton.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Skeleton : Enemy
 {
+    float timer;
+
+    [SerializeField] private float flipWaitTime;
+    [SerializeField] private float ledgeCheckX;
+    [SerializeField] private float ledgeCheckY;
+    [SerializeField] private LayerMask whatIsGround;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start()
     {
@@ -9,18 +16,42 @@ public class Skeleton : Enemy
         rb.gravityScale = 12f;
     }
 
-    // Update is called once per frame
-    protected override void Update()
+    protected override void UpdateEnemyStates()
     {
-        base.Update();
-        if (!isRecoiling)
+
+        switch (currentEnemyState)
         {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(PlayerController.Instance.transform.position.x, transform.position.y), speed * Time.deltaTime);
+            case EnemyStates.Skeleton_Idle:
+                Vector3 _ledgeCheckStart = transform.localScale.x > 0 ? new Vector3(ledgeCheckX, 0) : new Vector3(-ledgeCheckX, 0);
+                Vector2 _wallCheckDir = transform.localScale.x > 0 ? transform.right : -transform.right;
+
+                if (!Physics2D.Raycast(transform.position + _ledgeCheckStart, Vector2.down, ledgeCheckY, whatIsGround)
+                    || Physics2D.Raycast(transform.position, _wallCheckDir, ledgeCheckX, whatIsGround))
+                {
+                    ChangeState(EnemyStates.Skeleton_Flip);
+                }
+
+
+                if (transform.localScale.x > 0)
+                {
+                    rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+                } else
+                {
+                    rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
+                }
+                break;
+
+            case EnemyStates.Skeleton_Flip:
+                timer += Time.deltaTime;
+
+                if(timer > flipWaitTime)
+                {
+                    timer = 0;
+                    transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                    ChangeState(EnemyStates.Skeleton_Idle);
+                }
+                break;
         }
     }
 
-    public override void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitForce)
-    {
-        base.EnemyHit(_damageDone, _hitDirection, _hitForce);
-    }
 }
