df9cf760962e5ee40683a826e4914f7e6b047a8f
Fabrizio <fabrizio.orsini04@gmail.com>
2026-07-19 14:04:48 +0200

    Added Main Menu

---
+++ Assets/Script/MenuFadeController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuFadeController : MonoBehaviour
+{
+    private FadeUI fadeUI;
+    [SerializeField] private float fadeTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fadeUI = GetComponent<FadeUI>();
+        fadeUI.FadeUIOut(fadeTime);
+    }
+
+    public void CallFadeAndStartGame(string _sceneToLoad)
+    {
+        StartCoroutine(FadeAndStartGame(_sceneToLoad));
+    }
+
+    IEnumerator FadeAndStartGame(string _sceneToLoad)
+    {
+        fadeUI.FadeUIIn(fadeTime);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(_sceneToLoad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
